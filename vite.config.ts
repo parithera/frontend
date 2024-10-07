1ab73b75efe654002322413829e375a3ce704397
CedricHerzog <CedricHerzog@users.noreply.github.com>
2024-10-07 11:31:46 +0200

    Use modern scss compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 import EnvironmentPlugin from 'vite-plugin-environment';
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-import tailwind from "tailwindcss"
-import autoprefixer from "autoprefixer"
-
+import tailwind from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwind(), autoprefixer()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler' // or "modern"
+            }
+        },
+        postcss: {
+            plugins: [tailwind(), autoprefixer()]
+        }
     },
-  },
-  plugins: [
-    vue(),
-    EnvironmentPlugin('all'),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue(), EnvironmentPlugin('all')],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
-})
+});
