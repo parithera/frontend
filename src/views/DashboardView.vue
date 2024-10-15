509c602654c81e269982a27ac4992323bb833419
CedricHerzog <CedricHerzog@users.noreply.github.com>
2024-10-15 11:36:57 +0200

    Add incoming message on dashboard

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,3 +1,22 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Alert from '@/shadcn/ui/alert/Alert.vue';
+import AlertDescription from '@/shadcn/ui/alert/AlertDescription.vue';
+import AlertTitle from '@/shadcn/ui/alert/AlertTitle.vue';
+import { Terminal } from 'lucide-vue-next';
+</script>
 
-<template>Incoming</template>
+<template>
+    <div class="h-[calc(100vh-4rem)] w-screen flex items-center justify-center">
+        <div>
+            <Alert>
+                <Terminal class="h-4 w-4" />
+                <AlertTitle>Heads up!</AlertTitle>
+                <AlertDescription>
+                    <span>
+                        The dashboard is coming soon! You can use the project page for now.
+                    </span>
+                </AlertDescription>
+            </Alert>
+        </div>
+    </div>
+</template>
