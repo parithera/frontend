3c7d58451bea0399a08ca97ccda0b7bdfca037fe
Cédric HERZOG <cedric.herzpg@codeclarity.io>
2025-01-24 14:46:12 +0100

    Update group creation page

--- src/views/results/CreateGroups.vue
+++ src/views/results/CreateGroups.vue
@@ -1,31 +1,23 @@
 <script lang="ts" setup>
 import type { Project } from '@/repositories/types/entities/Project';
 import Button from '@/shadcn/ui/button/Button.vue';
-import type { ModelRef, Ref } from 'vue';
-import {
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from '@/shadcn/ui/form'
-import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from '@/shadcn/ui/select'
-
-import { toTypedSchema } from '@vee-validate/zod'
+import type { ModelRef } from 'vue';
 import { AutoForm } from '@/shadcn/ui/auto-form'
-import { toast } from '@/shadcn/ui/toast'
-import { h, ref } from 'vue'
+import {  onMounted } from 'vue'
 import * as z from 'zod'
-import { useForm } from 'vee-validate';
 import type { Group } from './types';
+import Card from '@/shadcn/ui/card/Card.vue';
+import CardHeader from '@/shadcn/ui/card/CardHeader.vue';
+import CardTitle from '@/shadcn/ui/card/CardTitle.vue';
+import CardContent from '@/shadcn/ui/card/CardContent.vue';
+import CardFooter from '@/shadcn/ui/card/CardFooter.vue';
+import { Icon } from '@iconify/vue';
+import Checkbox from '@/shadcn/ui/checkbox/Checkbox.vue';
+import Popover from '@/shadcn/ui/popover/Popover.vue';
+import PopoverTrigger from '@/shadcn/ui/popover/PopoverTrigger.vue';
+import PopoverContent from '@/shadcn/ui/popover/PopoverContent.vue';
+import AutoFormField from '@/shadcn/ui/auto-form/AutoFormField.vue';
+
 
 // Models
 const selected_project: ModelRef<Project> = defineModel('selected_project', { required: true });
@@ -33,64 +25,6 @@ const configure_analysis: ModelRef<boolean> = defineModel('configure_analysis', 
 const create_groups: ModelRef<boolean> = defineModel('create_groups', { required: true });
 const groups: ModelRef<Array<Group>> = defineModel('groups', { required: true });
 const file_type: ModelRef<string> = defineModel('file_type', { required: true });
-
-const group_number: Ref<number> = ref(0)
-
-// Get file names in selected_project.value.files?.name
-const files = selected_project.value.files?.map(file => file.name);
-
-type FilesEnumType = { [K in string]: K };
-
-const FileEnum: FilesEnumType = files
-    ? Object.fromEntries(files.map(file => [file, file])) as FilesEnumType
-    : {} as FilesEnumType;
-
-const schema = z.object({
-    groups: z.number().min(1).default(1).describe("How man groups do you want to create?"),
-})
-
-
-function onSubmit(values: Record<string, any>) {
-    group_number.value = values.groups
-
-    for (let index = 0; index < values.groups; index++) {
-        const group: Group = {
-            name: (index + 1).toString(),
-            files: []
-        }
-        groups.value.push(group)
-    }
-
-    toast({
-        title: 'Group created!'
-    })
-}
-
-
-const formSchema = toTypedSchema(z.object({
-    // email: z
-    //     .string({
-    //         required_error: 'Please select an email to display.',
-    //     })
-    //     .email(),
-}).passthrough())
-
-const { handleSubmit } = useForm({
-    validationSchema: formSchema,
-})
-
-const onFormSubmit = handleSubmit((values) => {
-    for (let t in values) {
-        for (let index = 0; index < groups.value.length; index++) {
-            const group = groups.value[index];
-            if (group.name == values[t]) {
-                if (file_type.value == 'fastq') groups.value[index].files.push(t + '.fastq.gz')
-                else groups.value[index].files.push(t + '.h5')
-            }
-        }
-    }
-    configure_analysis.value = true
-})
 
 function cleanFileName(name: string): string {
     if (name.includes('.fastq.gz')) {
@@ -102,45 +36,95 @@ function cleanFileName(name: string): string {
         return name.replace('.h5', '')
     }
 }
+
+function addGroup() {
+    const group: Group = {
+        name: "Group_" + groups.value.length,
+        files: []
+    }
+    groups.value.push(group)
+}
+
+function updateGroupName(values: Record<string, any>, index: number) {
+    groups.value[index].name = values.name.replace(" ", "_")
+}
+
+function deleteGroup(index: number) {
+    groups.value.splice(index, 1)
+}
+
+function addRemoveFileFromGroup(fileName:string, index: number) {
+    if (groups.value[index].files.includes(fileName)) {
+        const indexToRemove = groups.value[index].files.indexOf(fileName)
+        groups.value[index].files.splice(indexToRemove, 1)
+    }else{
+        groups.value[index].files.push(fileName)
+    }
+}
+
+const groupSchema = z.object({
+    name: z.string(),
+})
+
+onMounted(() => {
+    if (groups.value.length == 0) {
+        addGroup()
+    }
+})
 </script>
 <template>
-    <AutoForm class="w-2/3 space-y-6" :schema="schema" @submit="onSubmit">
-        <Button type="submit">
-            Create
-        </Button>
-    </AutoForm>
+    <div class="flex flex-col items-center gap-4 w-full">
+        <div class="flex gap-2 flex-wrap">
+            <Card v-for="(group, index) in groups" :key="group.name">
+                <CardHeader>
+                    <CardTitle class="flex gap-2">
+                        <Popover>
+                            <PopoverTrigger as-child>
+                                <div class="cursor-pointer flex gap-2">
+                                    {{ group.name }}
+                                    <Icon icon="material-symbols:edit"></Icon>
+                                </div>
 
-    <form class="w-2/3 space-y-6" @submit="onFormSubmit">
-        <FormField v-for="file in selected_project.files" v-slot="{ componentField }" :name="cleanFileName(file.name)">
-            <FormItem>
-                <FormLabel>{{ file.name }}</FormLabel>
+                            </PopoverTrigger>
+                            <PopoverContent class="w-80">
+                                <AutoForm class="w-2/3 space-y-6" :schema="groupSchema" :field-config="{
+                                    name: {
+                                        hideLabel: true,
+                                    },
+                                }" @submit="(payload) => { updateGroupName(payload, index) }">
+                                    <template #name="slotProps">
+                                        <div class="flex items-start gap-3">
+                                            <div class="flex-1">
+                                                <AutoFormField v-bind="slotProps" />
+                                            </div>
+                                            <div>
+                                                <Button type="submit">
+                                                    Update
+                                                </Button>
+                                            </div>
+                                        </div>
+                                    </template>
+                                </AutoForm>
+                            </PopoverContent>
+                        </Popover>
+                    </CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <div class="flex gap-2 items-center" id="files" v-for="file in selected_project.files"
+                        :key="file.name">
+                        <Checkbox @click="addRemoveFileFromGroup(file.name, index)"></Checkbox>
+                        <div>{{ cleanFileName(file.name) }}</div>
+                    </div>
+                </CardContent>
+                <CardFooter><Button @click="deleteGroup(index)" variant="destructive">Delete</Button></CardFooter>
+            </Card>
+            <div @click="addGroup" class="rounded-full px-2 py-1 h-fit w-fit bg-gray-100 cursor-pointer">+ Add group
+            </div>
+        </div>
+        <div class="flex justify-between w-1/2">
+            <Button @click="create_groups = false">Previous</Button>
+            <Button @click="configure_analysis = true">Next</Button>
+        </div>
 
-                <Select v-bind="componentField">
-                    <FormControl>
-                        <SelectTrigger>
-                            <SelectValue placeholder="Select a group" />
-                        </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                        <SelectGroup>
-                            <SelectItem v-for="group in groups" :key="group.name" :value="group.name">
-                                {{ group.name }}
-                            </SelectItem>
-                        </SelectGroup>
-                    </SelectContent>
-                </Select>
-                <FormDescription>
-                    You can manage email addresses in your
-                    <a href="/examples/forms">email settings</a>.
-                </FormDescription>
-                <FormMessage />
-            </FormItem>
-        </FormField>
-
-        <Button type="submit">
-            Submit
-        </Button>
-    </form>
-
-    <Button @click="create_groups = false">Previous</Button>
+    </div>
 </template>
