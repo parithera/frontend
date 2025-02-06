261a771513392757717d475111c0dc60f5d4a72d
Cédric HERZOG <cedric.herzog@codeclarity.io>
2025-02-06 16:15:41 +0100

    Remove reference to quality control vue in project

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,7 +1,6 @@
 <script lang="ts" setup>
 import { useStateStore } from '@/stores/state';
 import SelectProject from '@/views/projects/SelectProject.vue';
-import QualityControl from './results/QualityControl.vue';
 
 const state = useStateStore();
 state.$reset();
