3292ad41316ee71c493d9d5e616d3a5d7895bed2
Cédric HERZOG <cedric.herzog@codeclarity.io>
2025-02-09 14:39:44 +0100

    Improve QC page look

--- src/views/samples/QualityControl.vue
+++ src/views/samples/QualityControl.vue
@@ -8,6 +8,7 @@ import { onMounted, ref, type Ref } from 'vue';
 import { SampleRepository } from '@/repositories/SampleRepository';
 import { Icon } from '@iconify/vue/dist/iconify.js';
 import pako from 'pako';
+import Button from '@/shadcn/ui/button/Button.vue';
 
 const connectionStore = useConnectionStore();
 const authStore = useAuthStore();
@@ -74,15 +75,51 @@ function saveMultiQCReport() {
 
 <template>
     <div class="flex justify-end">
-        <Icon class="cursor-pointer m-4" width="1.5rem" icon="line-md:menu-to-close-transition" @click="() => router.back()" />
+        <Icon class="cursor-pointer m-4" width="1.5rem" icon="line-md:menu-to-close-transition"
+            @click="() => router.back()" />
     </div>
-    
-    <a href="#" @click.prevent="saveMultiQCReport" class="mt-4 text-blue-500 underline">Save MultiQC Report</a>
 
-    <div class="flex flex-wrap">
-        <img :src="'data:image/png;base64,' + svg_violin.trim()" />
-        <img class="w-1/2" :src="'data:image/png;base64,' + svg_variable_features.trim()" />
-        <img class="w-1/4" :src="'data:image/png;base64,' + svg_elbow.trim()" />
-        <img class="w-1/4" :src="'data:image/png;base64,' + svg_umap.trim()" />
+    <div class="flex justify-center">
+        <Button @click.prevent="saveMultiQCReport" class="rounded-full flex gap-2 items-center">
+            <Icon icon="material-symbols:save-outline"></Icon> Save MultiQC Report
+        </Button>
+    </div>
+
+    <div class="grid grid-cols-4 gap-y-12 mx-8 py-20">
+        <div class="flex flex-col items-center">
+            <img :src="'data:image/png;base64,' + svg_violin.trim()" style="width: 320px; height: 20em; object-fit: cover; object-position: 0 0;" />
+            <div class="font-bolder">Number of Genes</div>
+            <span class="text-sm text-center">The number of genes with at least 1 count in a cell. Calculated for all cells.</span>
+        </div>
+        <div class="flex flex-col items-center">
+            <img :src="'data:image/png;base64,' + svg_violin.trim()" style="width: 300px; height: 20em; object-fit: cover; object-position: 47.8% 0;" />
+            <div class="font-bolder">Total Counts</div>
+            <span class="text-sm text-center">Total number of counts for a cell.</span>
+        </div>
+        <div class="flex flex-col items-center">
+            <img :src="'data:image/png;base64,' + svg_violin.trim()" style="width: 300px; height: 20em; object-fit: cover; object-position: 95% 0;" />
+            <div class="font-bolder">Pct Counts MT</div>
+            <span class="text-sm text-center">Proportion of total counts for a cell which are mitochondrial.</span>
+        </div>
+        <div class="flex flex-col items-center">
+            <img :src="'data:image/png;base64,' + svg_variable_features.trim()" style="width: 375px; height: 20em; object-fit: cover; object-position: 0 0;" />
+            <div class="font-bolder">Feature Selection (Normalized)</div>
+            <span class="text-sm text-center">Dimensionality reduction of the dataset and only include the most informative genes.</span>
+        </div>
+        <div class="flex flex-col items-center">
+            <img :src="'data:image/png;base64,' + svg_variable_features.trim()" style="width: 375px; height: 20em; object-fit: cover; object-position: 100% 0;" />
+            <div class="font-bolder">Feature Selection (Not Normalized)</div>
+            <span class="text-sm text-center">Dimensionality reduction of the dataset and only include the most informative genes.</span>
+        </div>
+        <div class="flex flex-col items-center">
+            <img :src="'data:image/png;base64,' + svg_elbow.trim()" style="width: 375px; height: 20em;"/>
+            <div class="font-bolder">Principal Component Analysis (PCA)</div>
+            <span class="text-sm text-center">Contribution of single PCs to the total variance in the data.</span>
+        </div>
+        <div class="flex flex-col items-center">
+            <img :src="'data:image/png;base64,' + svg_umap.trim()" style="width: 375px; height: 20em;"/>
+            <div class="font-bolder">Uniform Manifold Approximation and Projection (UMAP)</div>
+            <span class="text-sm text-center">Dimensionality reduction in two dimensions for visualiztion with UMAP.</span>
+        </div>
     </div>
 </template>
